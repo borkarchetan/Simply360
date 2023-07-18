de8ef2ab2ed1fb5182c1380696749dd3372c235d
Lenovo <battleshipcomputer@gmail.com>
2023-07-18 18:50:09 +0530

    Adding...

---
+++ Simply360Assignment/src/com/question/_1/Bouquet.java
@@ -0,0 +1,32 @@
+package com.question._1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Bouquet {
+	private Map<String, Set<String>> bouquetMap;
+
+	public Bouquet() {
+		bouquetMap = new HashMap<>();
+	}
+
+	public void addFlower(String bouquetName, String flower, String color) {
+		bouquetMap.putIfAbsent(bouquetName, new HashSet<>());
+		Set<String> flowerSet = bouquetMap.get(bouquetName);
+		flowerSet.add(flower + " - " + color);
+	}
+
+	public String findBouquet(String flower, String color) {
+		for (Map.Entry<String, Set<String>> entry : bouquetMap.entrySet()) {
+			Set<String> flowerSet = entry.getValue();
+			for (String flowerInfo : flowerSet) {
+				if (flowerInfo.contains(flower + " - " + color)) {
+					return entry.getKey();
+				}
+			}
+		}
+		return "Output: None";
+	}
+}
---
+++ Simply360Assignment/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Simply360Assignment {
+}
